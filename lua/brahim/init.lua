f21e34053ab50b1514846ba30e71c7acb85a3cf6
BRAHIM elkartouch <118683350+belkarto@users.noreply.github.com>
2023-01-31 13:54:06 +0100

    Update init.lua

--- lua/brahim/init.lua
+++ lua/brahim/init.lua
@@ -1,6 +1,12 @@
-require("brahim.set")
 require("brahim.remap")
+require("brahim.set")
 -- set colorscheme after options
 vim.cmd('colorscheme nightfly')
 
 require("nvim-tree").setup()
+require("mason").setup{
+	providers = {
+		"mason.providers.client",
+		"mason.providers.registry-api",
+	}
+}

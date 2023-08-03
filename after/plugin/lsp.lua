dc4f938100782ae9d11c7a7fa4fdbc90452b2bcf
Brahim Elkartouch <belkarto@student.1337.ma>
2023-08-03 10:51:36 +0100

    feat(nvim-cmp): Accept suggestions with Enter key
    Changed the key mapping for the completion confirm function in nvim-cmp
    to accept suggestions with the Enter key (CR). This allows for a more
    intuitive and familiar way to confirm completions.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -11,7 +11,7 @@ local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-  ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<Cr>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
 })
 

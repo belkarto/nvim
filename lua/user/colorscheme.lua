5d68dd760c2277a57496e894d856196c40f5c3ff
brahim elkartouch <belkarto@student.1337.ma>
2023-10-12 19:42:46 +0100

    change defualt colorscheme and change tabe with

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,5 +1,6 @@
 --[[ local colorscheme = "tokyonight" ]]
-local colorscheme = "cityscape"
+--[[ local colorscheme = "cityscape" ]]
+local colorscheme = "night-owl"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then

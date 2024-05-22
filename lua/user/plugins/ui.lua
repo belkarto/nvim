e3f4ae7b86b8326021c69fbdbce28fda4672af07
brahim elkartouch <belkarto@student.1337.ma>
2024-05-22 08:25:45 +0000

    update

--- lua/user/plugins/ui.lua
+++ lua/user/plugins/ui.lua
@@ -975,13 +975,14 @@ local function notify()
 			},
 		},
 		opts = {
-			stages = "static",
-			timeout = 3000,
+			stages = "slide",
+			timeout = 2000,
+			render = "compact",
 			max_height = function()
 				return math.floor(vim.o.lines * 0.75)
 			end,
 			max_width = function()
-				return math.floor(vim.o.columns * 0.75)
+				return math.floor(vim.o.columns * 0.3)
 			end,
 			on_open = function(win)
 				vim.api.nvim_win_set_config(win, { zindex = 100 })
